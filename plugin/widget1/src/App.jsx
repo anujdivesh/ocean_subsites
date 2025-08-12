3fbe403cfa191a66f3d68f4df5d7e917fd0595aa
kunalsi398 <kunz.kunal@hotmail.com>
2025-08-13 10:50:52 +1200

    nuei_widget without auth

---
+++ plugin/widget1/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div style={{ 
+        backgroundColor: 'var(--color-background)', 
+        minHeight: '100vh',
+        transition: 'background-color 0.3s ease'
+      }}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/link1" element={<Link1 />} />
+          <Route path="/link2" element={<Link2 />} />
+          <Route path="/link3" element={<Link3 />} /> */}
+          {/* Redirect any unknown routes to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
